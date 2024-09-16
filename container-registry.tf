513a8208cb900636267aa5c722f9331c369e9cee
amriith <amrithjayadeep916@gmail.com>
2024-09-16 22:43:45 +1000

    first commit

---
+++ container-registry.tf
@@ -0,0 +1,8 @@
+
+resource "azurerm_container_registry" "container_registry" {
+  name                = var.app_name
+  resource_group_name = azurerm_resource_group.task82c.name
+  location            = var.location
+  admin_enabled       = true
+  sku                 = "Basic"
+}
